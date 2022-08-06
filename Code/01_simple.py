730793e1f976677c314ba8e3b606046d6273749e
amaargiru <war4one@gmail.com>
2022-08-06 17:09:24 +0500

    Initial commit

---
+++ Code/01_simple.py
@@ -0,0 +1,31 @@
+import asyncio
+from datetime import datetime
+
+
+async def fun1(a, b):
+    print(f"fun1 result: {a + b}")
+    await asyncio.sleep(1)
+    print("fun1 complete")
+
+
+async def fun2(a, b):
+    print(f"fun2 result: {a - b}")
+    await asyncio.sleep(1)
+    print("fun2 complete")
+
+
+async def main():
+    start_time = datetime.now()
+
+    task1 = asyncio.create_task(fun1(3, 2))
+    task2 = asyncio.create_task(fun2(3, 2))
+
+    await task1
+    await task2
+
+    duration_time = datetime.now() - start_time
+    print(f"Total duration time: {duration_time}")
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
